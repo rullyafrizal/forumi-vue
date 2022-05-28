6db0e2a27cf7769bf0fc649c6c435d05549b9737
rullyafrizal <rullyafrizal4@gmail.com>
2022-05-28 14:47:23 +0700

    finish main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,122 @@
 <template>
-  <div class="navbar bg-base-100">
-    <div class="flex-1">
-      <a class="btn btn-ghost normal-case font-bold text-2xl">
-        <img src="../assets/img/logo.png" class="h-8 pr-2" alt="">
-        Forumi
-      </a>
-    </div>
-    <div class="flex-none gap-2">
-      <div class="dropdown dropdown-end">
-        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-          <div class="w-10 rounded-full">
-            <img src="https://api.lorem.space/image/face?hash=33791" />
-          </div>
-        </label>
-        <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-          <li>
-            <a class="justify-between">
-              Profile
-              <span class="badge">New</span>
-            </a>
-          </li>
-          <li><a>Settings</a></li>
-          <li><a>Logout</a></li>
-        </ul>
+  <div>
+    <div class="navbar bg-base-100">
+      <div class="flex-1">
+        <a class="btn btn-ghost normal-case font-bold text-2xl">
+          <img src="../assets/img/logo.png" class="h-8 pr-2" alt="">
+          Forumi
+        </a>
+      </div>
+      <div class="flex-none gap-2">
+        <div class="dropdown dropdown-end">
+          <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img src="https://api.lorem.space/image/face?hash=33791" />
+            </div>
+          </label>
+          <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+            <li>
+              <a class="justify-between">
+                Profile
+                <span class="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="min-w-screen min-h-screen bg-gray-200 flex">
-
-    Hello World
+    <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+      <div class="grid grid-cols-4 gap-4">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Matematika</h2>
+            <p class="py-2">Bagaimana persamaan ini dirumuskan?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Answer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Matematika</h2>
+            <p class="py-2">Bagaimana persamaan ini dirumuskan?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Answer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Matematika</h2>
+            <p class="py-2">Bagaimana persamaan ini dirumuskan?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Answer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Matematika</h2>
+            <p class="py-2">Bagaimana persamaan ini dirumuskan?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Answer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Matematika</h2>
+            <p class="py-2">Bagaimana persamaan ini dirumuskan?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Answer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Matematika</h2>
+            <p class="py-2">Bagaimana persamaan ini dirumuskan?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Answer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Matematika</h2>
+            <p class="py-2">Bagaimana persamaan ini dirumuskan?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Answer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Matematika</h2>
+            <p class="py-2">Bagaimana persamaan ini dirumuskan?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Answer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
