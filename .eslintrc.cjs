dbc456aa137d6e705eca81c5545f8f3ffc30acc0
rullyafrizal <rullyafrizal4@gmail.com>
2022-05-28 12:39:34 +0700

    initial boilerplate

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'vue/setup-compiler-macros': true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
