5b0502b3fb8698bceeaccb30960cba1dd699064e
rullyafrizal <rullyafrizal4@gmail.com>
2022-05-29 11:39:42 +0700

    adjust responsiveness

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,18 +10,6 @@
               Home
             </router-link>
           </li>
-<!--          <li>-->
-<!--            <div class="flex items-center">-->
-<!--              <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>-->
-<!--              <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">Projects</a>-->
-<!--            </div>-->
-<!--          </li>-->
-<!--          <li aria-current="page">-->
-<!--            <div class="flex items-center">-->
-<!--              <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>-->
-<!--              <span class="ml-1 text-sm font-medium text-gray-400 md:ml-2 dark:text-gray-500">Flowbite</span>-->
-<!--            </div>-->
-<!--          </li>-->
         </ol>
       </nav>
     </div>
@@ -39,7 +27,7 @@
         </div>
       </form>
     </div>
-    <div class="pt-5 pb-10 px-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    <div class="pt-5 pb-10 px-5 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
       <div v-for="card in 12" :key="card" class="grid grid-cols-4 gap-4">
         <QuestionCard/>
       </div>
